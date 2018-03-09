f911d2fd6536e436fa7db1b9deb121b11df710af
Michel Palourdio <mpalourdio@gmail.com>
2018-03-09 15:03:38 +0100

    Bootstrap project

---
+++ main.ts
@@ -0,0 +1,4 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgRestUploadModule } from './src/ng-rest-upload.module';
+
+platformBrowserDynamic().bootstrapModule(NgRestUploadModule);
